caacce618ce05a7cb979120bd4288b8b593b87f0
adcastiblanco <angelcastiblanco96@gmail.com>
2020-12-18 06:48:24 -0500

    set variables of colors

---
+++ src/res/styles/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Colors = {
+  greenPrimary: "#98CA3F",
+  bluePrimary: "#0A4878",
+  blueSecondary: "#638CAB",
+  grayPrimary: "#DEE7ED",
+  graySecondary: "#F5F8FA",
+  statusRed: "#D73B2F",
+};
